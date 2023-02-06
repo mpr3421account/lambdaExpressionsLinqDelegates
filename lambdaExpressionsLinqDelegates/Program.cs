db6b955ae9898485ed3b61bae7abada0a357abf6
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-02-06 17:51:54 -0300

    IComparable implementation

--- lambdaExpressionsLinqDelegates/Program.cs
+++ lambdaExpressionsLinqDelegates/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using lambdaExpressionsLinqDelegates.Entities;
+
+List<Product> list = new List<Product>();
+
+list.Add(new Product("TV", 900.00));
+list.Add(new Product("Notebook", 1200.00));
+list.Add(new Product("Tablet", 450.00));
+
+list.Sort();
+
+foreach(Product p in list)
+{
+    Console.WriteLine(p);
+}
