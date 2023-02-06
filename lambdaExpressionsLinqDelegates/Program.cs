f62a19a8cf896b80f868d44aeb9f7297c5bf9702
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-02-06 18:04:47 -0300

    Inline lambda expression

--- lambdaExpressionsLinqDelegates/Program.cs
+++ lambdaExpressionsLinqDelegates/Program.cs
@@ -6,9 +6,9 @@ list.Add(new Product("TV", 900.00));
 list.Add(new Product("Notebook", 1200.00));
 list.Add(new Product("Tablet", 450.00));
 
-Comparison<Product> comp = (p1,p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+//Comparison<Product> comp = (p1,p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
 
-list.Sort(comp);//chamando a função CompareProducts - Referência para a Função - Delegate
+list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));//chamando a função CompareProducts - Referência para a Função - Delegate
 
 foreach(Product p in list)
 {
