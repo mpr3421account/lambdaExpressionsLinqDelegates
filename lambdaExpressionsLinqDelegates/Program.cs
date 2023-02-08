e9e0a58b31cd5f02b8c509913ad5ab0c327fb7d6
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-02-08 11:30:26 -0300

    Predicate (exemplo com RemoveAll)Predicate

--- lambdaExpressionsLinqDelegates/Program.cs
+++ lambdaExpressionsLinqDelegates/Program.cs
@@ -4,15 +4,22 @@ lista somente aqueles cujo preço mínimo seja 100.*/
 
 using lambdaExpressionsLinqDelegates.Entities;
 
-List<Product> list = new List<Product>();
-
-list.Add(new Product("TV", 900.00));
-list.Add(new Product("Mouse", 50.00));
-list.Add(new Product("Tablet", 350.50));
-list.Add(new Product("HD Case", 80.90));
-
-list.RemoveAll(p => p.Price >= 100.0);
-foreach(Product p in list)
+internal class Program
 {
-    Console.WriteLine(p);
+    private static void Main(string[] args)
+    {
+        List<Product> list = new List<Product>();
+
+        list.Add(new Product("TV", 900.00));
+        list.Add(new Product("Mouse", 50.00));
+        list.Add(new Product("Tablet", 350.50));
+        list.Add(new Product("HD Case", 80.90));
+
+        list.RemoveAll(ProducTest);
+        foreach (Product p in list)
+        {
+            Console.WriteLine(p);
+        }
+    }
+    public static bool ProducTest(Product p) { return p.Price >= 100.0; }
 }
